9c762834411f0c10c56a583270cff19fa8a3d73b
yasin <yasinsheikhofficial@gmail.com>
2026-03-31 07:25:57 +0530

    page.tsx changed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [title, setTitle] = useState("");
 
   // ✅ Dynamic API URL (IMPORTANT FIX)
-  const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+  const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://backend:5000";
 
   const fetchTasks = async () => {
     try {

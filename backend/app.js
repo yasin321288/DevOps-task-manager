5cf94038466d01092fa578b40810b8822ae2c3c0
yasin <yasinsheikhofficial@gmail.com>
2026-03-31 08:22:40 +0530

    cors alternative used in app.js in backend

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,13 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
 app.use(express.json());
 
 app.use("/", taskRoutes);
